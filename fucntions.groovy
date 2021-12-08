24598ee1bd92bb2df5baef8f235c08345add93d2
babeysekera <revolutiongc.lk@gmail.com>
2021-12-08 21:42:31 +0530

    file creation ssh

--- fucntions.groovy
+++ fucntions.groovy
@@ -4,13 +4,14 @@ def testConfig() {
 
 def buildConfig() {
     sh 'echo "build completed - SUCCESS"'
-    echo "build version is ${APP_VERSION}"}
+    echo "build version is ${APP_VERSION}"
+}
 
 def deployConfig() {
     sh 'echo "deployment completed - SUCCESS"'
     echo "credentials are ${CREDENTIALS}"
     sh '''ssh root@192.168.220.132 'bash -s' <<'ENDSSH'
-  pwd
+  touch test.txt
 ENDSSH
 '''
 }
